4ffbe44f9a3d79bd57ccba3f4f3b93158f9c1d43
Taras Shevchenko <shev-taras@ya.ru>
2022-03-02 11:36:30 +0400

    the functionality of the web component works

--- app-web.js
+++ app-web.js
@@ -53,57 +53,60 @@ class MyForm extends HTMLElement {
     }
     companySearch() {
         document.querySelector('.company__name');
-const dataList = document.getElementById('json-datalist');
-const input = document.getElementById('ajax');
-const url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/party";
-const token = "d5b380a8f91d1902eb4b50b39a1e7111a459d70f";
-input.addEventListener('keyup', () => {
-    const options = {
-        method: "POST",
-        mode: "cors",
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-            "Authorization": "Token " + token
-        },
-        body: JSON.stringify({
-            query: input.value,
-            count: 20})
-    }
-    fetch(url, options)
-        .then(response => response.json())
-        .then((result) => {
-            console.log(result)
-            console.log(input.value)
-            let cloneResult = JSON.parse(JSON.stringify(result.suggestions));
-            const option = document.createElement('option')
-
-            cloneResult.forEach(item => {
-
-                option.value = item.value
-                dataList.appendChild(option)
-                input.placeholder = "e.g. datalist"
-                input.placeholder = "Loading options..."
-            })
-            if(input.value === option.value) {
-                const briefliCompany = document.querySelector('.company__briefly');
-                const completeCompany = document.querySelector('.company__complete');
-                const companyINN =  document.querySelector('.company__INN');
-                const companyAddress =document.querySelector('.company__address');
-                const [{...currentCompany}] = cloneResult;
-                briefliCompany.value = currentCompany.data.name.short_with_opf;
-                completeCompany.value = currentCompany.data.name.full_with_opf;
-                companyINN.value = `${currentCompany.data.inn}/${!currentCompany.data.kpp ? 'не имеет' : currentCompany.data.kpp}`;
-                companyAddress.value = currentCompany.data.address.unrestricted_value;
-
+        const dataList = document.getElementById('json-datalist');
+        const input = document.getElementById('ajax');
+        const url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/party";
+        const token = "d5b380a8f91d1902eb4b50b39a1e7111a459d70f";
+        input.addEventListener('keyup', () => {
+            const options = {
+                method: "POST",
+                mode: "cors",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json",
+                    "Authorization": "Token " + token
+                },
+                body: JSON.stringify({
+                    query: input.value,
+                    count: 20})
             }
-            }
-        )
-        .catch(error => console.log("error", error));
-})
+            fetch(url, options)
+                .then(response => response.json())
+                .then((result) => {
+
+
+                        let cloneResult = JSON.parse(JSON.stringify(result.suggestions));
+                        const newArray = cloneResult.map(item=> item.unrestricted_value)
+
+                        console.log(newArray)
+                        newArray.forEach(item => {
+                            const option = document.createElement('option')
+                            option.value = item
+                            dataList.appendChild(option)
+                            input.placeholder = "e.g. datalist"
+                            input.placeholder = "Loading options..."
+                            if(input.value === option.value) {
+                                const briefliCompany = document.querySelector('.company__briefly');
+                                const completeCompany = document.querySelector('.company__complete');
+                                const companyINN =  document.querySelector('.company__INN');
+                                const companyAddress =document.querySelector('.company__address');
+                                const [{...currentCompany}] = cloneResult;
+                                briefliCompany.value = currentCompany.data.name.short_with_opf;
+                                completeCompany.value = currentCompany.data.name.full_with_opf;
+                                companyINN.value = `${currentCompany.data.inn}/${!currentCompany.data.kpp ? 'не имеет' : currentCompany.data.kpp}`;
+                                companyAddress.value = currentCompany.data.address.unrestricted_value;
+                                return  option.closest
+                            }
+
+                        })
+
+                    }
+                )
+                .catch(error => console.log("error", error));
+        })
 
     }
 
 }
 
-    customElements.define("my-form", MyForm);
+customElements.define("my-form", MyForm);
